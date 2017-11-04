a690d5ebcf25b3c6338b87d461cc02732209f707
Saurav Saha <contact.srvsaha@gmail.com>
2017-11-04 12:15:29 +0530

    Preprocessing Texts

---
+++ Codes/text_preprocessing.py
@@ -0,0 +1,83 @@
+#
+#   @author      : SRvSaha
+#   Filename     : text_preprocessing.py
+#   Timestamp    : 12:12 03-November-2017 (Saturday)
+#   Email        : contact [dot] srvsaha [at] gmail.com
+
+import sys
+import numpy as np
+import json
+# Tokenizer is a class which is a high level
+# API in keras for dealing with text preprocessing
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+
+def load_features_images_captions(captions_file, features_file):
+    # For loading the image features extracted using ResNet50
+    features = np.load(features_file)
+    # To store the id(name) of the images
+    images = []
+    # To store the corresponding caption of the images
+    texts = []
+    # The Input Captions file consists of Image Name and the
+    # corresponding caption separated by space
+    with open(captions_file) as f:
+        # For each line in the captions file
+        for line in f:
+            # Removing the trailing and leading whitespaces
+            # and splitting using space so that there are two
+            # token : Image Name and the corresponding caption
+            tokens = line.strip().split()
+            # Image names are inserted into the list of images
+            images.append(tokens[0])
+            # Since all the words of the caption are also separated
+            # by space so they are joined again to recompose the string
+            # and then caption in inserted into the list of texts
+            texts.append(" ".join(tokens[1:]))
+    # Returns the Image Features, Image Name and Corresponding Caption
+    # return features, images, texts
+    return texts
+
+
+def preprocess_text(texts):
+    # Instantiating the Tokenizer Class
+    tokenizer = Tokenizer()
+    # Fitting the tokenizer object on List of the texts/captions
+    tokenizer.fit_on_texts(texts)
+    # Converts each word to integer sequence i.e, basically it assigns
+    # an integer id to each word so, duplicates of any kind is removed
+    sequences = tokenizer.texts_to_sequences(texts)
+    # To check what is the length of the sequence with the max vector
+    # so that we can have an ideas what should be the maximum length of the
+    # padding that we are using
+    max_sequence_length = max([len(seq) for seq in sequences])
+    # Padding is done since all the caption are not of same length
+    # All the captions were observed to be small so, size of 16 is chosen
+    # for the overall unified length of all captions.
+    # If the length of the caption is less than 16, it is padded with zeros
+    # in the front (by default)
+    captions_after_padding = pad_sequences(sequences, maxlen=16)
+    # word.index returns a dictionary where the key is the word and the value
+    # is the index that is assigned/ by which the word sequence is changed into
+    #  integer sequence
+    vocabulary = tokenizer.word_index
+    # Adding the entry of end of sentence into the dictionary as a values of 0
+    # so as to keep track of the end of the sentence.
+    vocabulary['<eos>'] = 0
+    # Returns the dictionary/vocabulary where each word is mapped with it's
+    # sequence
+    print(len(vocabulary))
+    return vocabulary
+
+
+def saving_vocabulary(vocab):
+    with open('vocabulary.json', 'w') as f:
+        f.write(json.dumps(vocab))
+    print("Successfully dumped as JSON :)")
+
+
+if __name__ == "__main__":
+    vocabulary = preprocess_text(
+        load_features_images_captions(sys.argv[1], sys.argv[2]))
+    saving_vocabulary(vocabulary)
